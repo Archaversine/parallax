5387c93e4ad22f675566f666a685f53405a1d8f4
Archaversine <adam.brohl.w@gmail.com>
2026-02-28 19:42:01 -0500

    Added planet object

---
+++ planet.gd
@@ -0,0 +1,47 @@
+@tool
+extends Node2D
+
+enum PlanetType { 
+	RANDOM,
+	GREEN_BLUE, 
+	EARTH, 
+	GREEN_PURPLE, 
+	RED_ORANGE, 
+	DARK_BLUE, 
+	DESERT, 
+	DARK_DESERT, 
+	WATER, 
+	FIRE, 
+	PURPLE 
+}
+
+@export var planet_texture: PlanetType:
+	set(value):
+		planet_texture = value
+		update_texture()
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+var planet_textures = {
+	PlanetType.EARTH: preload("res://assets/sprites/planets/planet03.png"),
+	PlanetType.GREEN_BLUE: preload("res://assets/sprites/planets/planet00.png"),
+	PlanetType.GREEN_PURPLE: preload("res://assets/sprites/planets/planet01.png"),
+	PlanetType.RED_ORANGE: preload("res://assets/sprites/planets/planet02.png"),
+	PlanetType.DARK_BLUE: preload("res://assets/sprites/planets/planet04.png"),
+	PlanetType.DESERT: preload("res://assets/sprites/planets/planet05.png"),
+	PlanetType.DARK_DESERT: preload("res://assets/sprites/planets/planet06.png"),
+	PlanetType.WATER: preload("res://assets/sprites/planets/planet07.png"),
+	PlanetType.FIRE: preload("res://assets/sprites/planets/planet08.png"),
+	PlanetType.PURPLE: preload("res://assets/sprites/planets/planet09.png"),
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	update_texture()
+
+func update_texture():
+	if not sprite:
+		sprite = get_node_or_null("Sprite2D")
+	
+	if sprite and planet_texture:
+		sprite.texture = planet_textures.get(planet_texture)
